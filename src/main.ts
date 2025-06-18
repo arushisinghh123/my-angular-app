11f8733a42006fe346cea00600e6b1789011b205
arushisinghh123 <sarushi2025@gmail.com>
2025-06-18 11:40:34 +0530

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config';
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch(err => console.error(err));
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+  ]
+};
