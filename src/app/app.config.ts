33d58a18f1f4bbe10b9a542188acd93445b5b749
arushisinghh123 <sarushi2025@gmail.com>
2025-06-18 11:09:11 +0530

    Updated app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes)
-    //importProvidersFrom(BrowserAnimationsModule)  // ✅ this is fine
+    provideRouter(routes),
+    importProvidersFrom(BrowserAnimationsModule)  // ✅ this is fine
   ]
 };
