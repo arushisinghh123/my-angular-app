a73c158a5dd85deea3e7b732a1a2e536d976e613
arushisinghh123 <sarushi2025@gmail.com>
2025-06-18 10:30:32 +0530

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { appConfig } from './app/app.config';
 
 bootstrapApplication(AppComponent, appConfig)
