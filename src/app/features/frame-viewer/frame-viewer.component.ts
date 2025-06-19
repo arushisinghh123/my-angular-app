d571179c240b036d4386a7d9f73abb3a593f878a
arushisinghh123 <sarushi2025@gmail.com>
2025-06-19 08:04:47 +0530

    Updated frame-viewer.component.ts

--- src/app/features/frame-viewer/frame-viewer.component.ts
+++ src/app/features/frame-viewer/frame-viewer.component.ts
@@ -4,6 +4,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatChipsModule } from '@angular/material/chips';
 import { Subject } from 'rxjs';
 import { takeUntil, finalize } from 'rxjs/operators';
@@ -19,6 +22,9 @@ import { ImageService, FrameMetadata } from '../../core/services/image.service';
     MatCardModule,
     MatIconModule,
     MatProgressSpinnerModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatButtonModule,
     MatChipsModule
   ],
